f5a9ee47d0bdf0023c7cea9ee8c61873d2525942
Gillian <gillian@localhost.localdomain>
2020-09-11 16:32:45 +0300

    Fibonacci Algorithm and Longest Palindromic Substring Algorithm

---
+++ fibonacci.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Fibonacci
+{
+    public static void main(String[] args)
+    {
+        long n, a = 0, b = 0, c = 1;
+        System.out.print("Fibonacci Series:");
+        for(int i = 1; i<=100; i++){
+            a = b;
+            b = c;
+            c = a+b;
+            System.out.print(a+" ");
+        }
+    }
+}
